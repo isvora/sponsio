3996ccd6971a29d6a4f28355485c2c941ac8edf6
isvora <isvoranandrei25@gmail.com>
2022-11-21 14:52:34 +0200

    Initial commit

---
+++ src/main/java/eridanus/sponsio/SponsioApplication.java
@@ -0,0 +1,13 @@
+package eridanus.sponsio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SponsioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SponsioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eridanus/sponsio/SponsioApplicationTests.java
@@ -0,0 +1,13 @@
+package eridanus.sponsio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SponsioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
