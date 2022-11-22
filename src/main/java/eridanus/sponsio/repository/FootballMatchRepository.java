dc396713f04d92e87647b8c0fb17d4e3aaa84bd2
isvora <isvoranandrei25@gmail.com>
2022-11-22 19:09:50 +0200

    Football repository

--- src/main/java/eridanus/sponsio/repository/FootballMatchRepository.java
+++ src/main/java/eridanus/sponsio/repository/FootballMatchRepository.java
@@ -2,6 +2,8 @@ package eridanus.sponsio.repository;
 
 import eridanus.sponsio.database.FootballMatch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FootballMatchRepository extends JpaRepository<FootballMatch, Long> {
 }
--- src/main/java/eridanus/sponsio/repository/FootballOddsRepository.java
+++ src/main/java/eridanus/sponsio/repository/FootballOddsRepository.java
@@ -2,6 +2,8 @@ package eridanus.sponsio.repository;
 
 import eridanus.sponsio.database.FootballOdds;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FootballOddsRepository extends JpaRepository<FootballOdds, Long> {
 }
